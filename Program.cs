b67c92ad677983c6bf93724de1532c6c6af6c914
AphroditeF <a.flora@outlook.com>
2023-06-21 04:09:58 +0300

    First Program using arguments from terminal

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string myString = "test";
+            myString += ". second part.";
+            Console.WriteLine(myString);
+
+            string[] myStringArr = myString.Split(".");
+            Console.WriteLine(myStringArr[0]);
+            Console.WriteLine(myStringArr[1]);
+            Console.WriteLine(myStringArr[2]);
+
+            //ghjgfhjj
+
+            // See https://aka.ms/new-console-template for more information
+            Console.WriteLine(args[0]);
+        }
+    }
+}
